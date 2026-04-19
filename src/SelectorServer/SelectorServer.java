63932ad1dde29b70c45b77cb958eda00c5cfccb2
Arieli <kubikapb@gmail.com>
2026-04-19 15:19:21 +0200

    explanation of SelectorServer

--- src/SelectorServer/SelectorServer.java
+++ src/SelectorServer/SelectorServer.java
@@ -17,31 +17,31 @@ public class SelectorServer
         try(ServerSocketChannel serverChannel = ServerSocketChannel.open())
         {
             serverChannel.bind(new InetSocketAddress(5678));
-            serverChannel.configureBlocking(false);
+            serverChannel.configureBlocking(false); //!! ważne nie blokuje jesli nie ma odpowiedzi na operacji wejscia wyjscia
 
             Selector selector = Selector.open();
-            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);   // selektor bedzie akceptowac klientow
 
             while (true)
             {
-                selector.select();
-                Set<SelectionKey> selectedKeys = selector.selectedKeys();
-                Iterator<SelectionKey> iterator = selectedKeys.iterator();
+                selector.select();      //odczytuje z selektora
+                Set<SelectionKey> selectedKeys = selector.selectedKeys(); //zwraca zestaw kluczy
+                Iterator<SelectionKey> iterator = selectedKeys.iterator(); // iterator pozwlajacy przechodzic kolekje (jak for ale mozna usunac w trakcie )
 
-                while (iterator.hasNext())
+                while (iterator.hasNext()) // dopoki mamy zdarzenia
                 {
-                    SelectionKey key = iterator.next();
-                    iterator.remove();
+                    SelectionKey key = iterator.next();  // pobieramy nastepne zdarzenie
+                    iterator.remove();  //usuwamy z zestawu zdarzenie
 
                     if(key.isAcceptable())
                     {
-                        SocketChannel clientChannel = serverChannel.accept();
-                        System.out.println("Client connected: " + clientChannel.getRemoteAddress());
-                        clientChannel.configureBlocking(false);
-                        clientChannel.register(selector, SelectionKey.OP_READ);
+                        SocketChannel clientChannel = serverChannel.accept(); // akceptujemy klienta
+                        System.out.println("Client connected: " + clientChannel.getRemoteAddress()); //wyswietlamy adres klienta
+                        clientChannel.configureBlocking(false); // nie blokuje jesli nie ma odpowiedzi na operacji wejscia wyjscia
+                        clientChannel.register(selector, SelectionKey.OP_READ); // obserwujemy kanal i zwracamy uwage na to czy zostalo cos wyslane
                     } else if (key.isReadable())
                     {
-                        echoData(key);
+                        echoData(key); // wyswietlamy dane
                     }
                 }
             }
@@ -53,22 +53,22 @@ public class SelectorServer
 
     private static void echoData(SelectionKey key) throws IOException
     {
-        SocketChannel clientChannel = (SocketChannel) key.channel();
+        SocketChannel clientChannel = (SocketChannel) key.channel(); // pobiermay chanel z klucza
         ByteBuffer buffer = ByteBuffer.allocate(1024);
 
-        int bytesRead = clientChannel.read(buffer);
+        int bytesRead = clientChannel.read(buffer); // czytamy dane z kanalu
         if(bytesRead > 0)
         {
-            buffer.flip();
+            buffer.flip(); // brzelaczamy  w tryb odczytu
             byte[] data = new byte[buffer.remaining()];
-            buffer.get(data);
-            String message = "Echo: " + new String(data);
-            clientChannel.write(ByteBuffer.wrap(message.getBytes()));
+            buffer.get(data); // zapisujemy dane do tablicy
+            String message = "Echo: " + new String(data); // tworzymy wiadomosc
+            clientChannel.write(ByteBuffer.wrap(message.getBytes())); // wysylamy wiadomosc
         } else if (bytesRead == -1)
         {
             System.out.println("Client disconnected: " + clientChannel.getRemoteAddress());
-            key.cancel();
-            clientChannel.close();
+            key.cancel(); //usuwamy klucz
+            clientChannel.close(); //zamyka kanal
         }
     }
 }
